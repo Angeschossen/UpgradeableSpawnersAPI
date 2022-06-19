2a413ba7970c71d2a2ef456b54672e729d1aa1a1
David Schwalm <angeschossen2019@gmail.com>
2022-06-19 15:29:37 +0200

    getSpawner

--- src/main/java/me/angeschossen/upgradeablespawners/api/UpgradeableSpawnersAPI.java
+++ src/main/java/me/angeschossen/upgradeablespawners/api/UpgradeableSpawnersAPI.java
@@ -1,7 +1,11 @@
 package me.angeschossen.upgradeablespawners.api;
 
+import me.angeschossen.upgradeablespawners.api.spawner.Spawner;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
 
@@ -21,5 +25,31 @@ public class UpgradeableSpawnersAPI {
     public static boolean isSpawnedBySpawner(@NotNull Entity entity) {
         Objects.requireNonNull(entity, "entity can't be null");
         return false;
+    }
+
+    /**
+     * Get an upgradeable spawner by at a location.
+     *
+     * @param location The location
+     * @return null, if the block at the given location is no upgradeable spawner.
+     */
+    @Nullable
+    public static Spawner getSpawner(@NotNull Location location) {
+        Objects.requireNonNull(location, "location can't be null");
+
+        return null;
+    }
+
+    /**
+     * Get an upgradeable spawner from a block.
+     *
+     * @param block The location
+     * @return null, if the block is no spawner or no upgradeable spawner.
+     */
+    @Nullable
+    public static Spawner getSpawner(@NotNull Block block) {
+        Objects.requireNonNull(block, "block can't be null");
+
+        return null;
     }
 }
