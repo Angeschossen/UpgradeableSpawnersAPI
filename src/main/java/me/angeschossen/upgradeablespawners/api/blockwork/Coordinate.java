18a095909d32678ac13170ead2d4560fbfff461e
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2023-08-25 12:25:13 +0200

    ignore

--- src/main/java/me/angeschossen/upgradeablespawners/api/blockwork/Coordinate.java
+++ src/main/java/me/angeschossen/upgradeablespawners/api/blockwork/Coordinate.java
@@ -4,18 +4,52 @@ import org.bukkit.Location;
 import org.bukkit.World;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Represents a position in a loaded world.
+ */
 public interface Coordinate {
+    /**
+     * Get block x position.
+     *
+     * @return block x
+     */
     int getX();
 
+    /**
+     * Get block y position.
+     *
+     * @return block y
+     */
     int getY();
 
+    /**
+     * Get block z position.
+     *
+     * @return block z
+     */
     int getZ();
 
+    /**
+     * Get chunk X identifier.
+     * @return chunk X
+     */
     int getChunkX();
 
+    /**
+     * Get chunk Z identifier.
+     * @return chunk Z
+     */
     int getChunkZ();
 
+    /**
+     * Get the world of this coordinate.
+     * @return never null
+     */
     @NotNull World getWorld();
 
-    @NotNull Location getLocation();
+    /**
+     * Creates a new location instance.
+     * @return coordinate to location
+     */
+    @NotNull Location toLocation();
 }
--- src/main/java/me/angeschossen/upgradeablespawners/api/economy/EconomyProvider.java
+++ src/main/java/me/angeschossen/upgradeablespawners/api/economy/EconomyProvider.java
@@ -2,6 +2,9 @@ package me.angeschossen.upgradeablespawners.api.economy;
 
 import com.github.angeschossen.pluginframework.api.economy.EconomyIntegration;
 
+/**
+ * Allows you to implement your own economy provider.
+ */
 public interface EconomyProvider extends EconomyIntegration {
 
 }
